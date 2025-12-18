2d49de05bc5654259b488f7ca97db28a551f6fdd
Zeljka Cotra <zeljka.cotra@emeal.nttdata.com>
2025-12-18 13:33:18 +0100

    resolve issues with git

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+    open: true,
+    port: 8080,
+  },
+};
